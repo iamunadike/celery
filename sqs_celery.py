244bac1d33399ad15cd9cf40010cb499e26635f9
Michael You <48380822+iamunadike@users.noreply.github.com>
2025-01-04 13:18:53 +0100

    Update sqs_celery.py

--- sqs_celery.py
+++ sqs_celery.py
@@ -1,41 +1,6 @@
-# Script you sent to me
-from celery import Celery
-from kombu import Exchange, Queue
-
-app = Celery(
-    'cel',
-    broker='sqs://',
-    backend='celery_s3.backends.S3Backend',
-    broker_transport_options={
-        'visibility_timeout': 3600,
-        'task_create_missing_queues': False,
-        'region': 'eu-central-1',
-        'queue_name_prefix': 'cel-',
-        'endpoint_url': 'https://sqs.eu-central-1.amazonaws.com'
-    },
-    broker_connection_retry_on_startup=True
-)
-
-# Define explicit queues
-app.conf.task_queues = (
-    Queue('queue1', Exchange('exchange1'), routing_key='route1'),
-    Queue('queue2', Exchange('exchange2'), routing_key='route2'),
-)
-
-# Add S3 backend settings
-app.conf.update(
-    CELERY_S3_BACKEND_SETTINGS={
-        'bucket': 'celery_back',
-        'region': 'eu-central-1',
-        'endpoint_url': 'https://s3.eu-central-1.amazonaws.com'
-    }
-)
 
 
-@app.task
-def add(x, y):
-    return x + y
-
+# USE THIS FOR PASTEBIN https://pastebin.com/K2E7qRBk
 
 # Solution for BUG
 
@@ -83,6 +48,7 @@ app.conf.broker_connection_retry_on_startup = True
 def add(x, y):
     return x + y
 
+# YOU CAN NOW USE THIS FOR PASTEBIN: https://pastebin.com/Nz9HZuAB
 
 # Try executing with this code 
 from celery_tasks import add
